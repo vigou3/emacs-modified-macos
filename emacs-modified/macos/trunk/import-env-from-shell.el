221555510f484da95cfbb3b5525458ccd642b398
vigou3 <vigou3@5a16c0e7-0f79-4c8a-b882-1e6d53282a5a>
2014-10-22 02:51:47 +0000

    'import-env-from-shell' updated along Steve Purcell's current code of for exec-path-from-shell.el. Should work better at importing the environment for shells other than bash.

--- emacs-modified/macos/trunk/import-env-from-shell.el
+++ emacs-modified/macos/trunk/import-env-from-shell.el
@@ -1,6 +1,6 @@
 ;;; import-env-from-shell.el --- Make Emacs use the environment set up by the user's shell
 
-;; Copyright (C) 2013 Vincent Goulet
+;; Copyright (C) 2013-2014 Vincent Goulet
 
 ;; Author: Vincent Goulet
 
@@ -34,18 +34,118 @@
 (defcustom import-env-from-shell-variables
   '("PATH" "MANPATH" "LANG")
   "List of environment variables which are copied from the shell."
+  :type '(repeat (string :tag "Environment variable"))
   :group 'import-env-from-shell)
+
+(defvar import-env-from-shell-debug nil
+  "Display debug info when non-nil.")
+
+(defun import-env-from-shell--double-quote (s)
+  "Double-quote S, escaping any double-quotes already contained in it."
+  (concat "\"" (replace-regexp-in-string "\"" "\\\\\"" s) "\""))
+
+(defun import-env-from-shell--tcsh-p (shell)
+  "Return non-nil if SHELL appears to be tcsh."
+  (string-match "tcsh$" shell))
+
+(defun import-env-from-shell--login-arg (shell)
+  "Return the name of the --login arg for SHELL."
+  (if (import-env-from-shell--tcsh-p shell) "-d" "-l"))
+
+(defcustom import-env-from-shell-arguments
+  (list (import-env-from-shell--login-arg (getenv "SHELL")) "-i")
+  "Additional arguments to pass to the shell.
+
+The default value denotes an interactive login shell."
+  :type '(repeat (string :tag "Shell argument"))
+  :group 'import-env-from-shell)
+
+(defun import-env-from-shell--debug (msg &rest args)
+  "Print MSG and ARGS like `message', but only if debug output is enabled."
+  (when import-env-from-shell-debug
+    (apply 'message msg args)))
+
+(defun import-env-from-shell--standard-shell-p (shell)
+  "Return non-nil iff SHELL supports the standard ${VAR-default} syntax."
+  (not (string-match "\\(fish\\|tcsh\\)$" shell)))
+
+(defun import-env-from-shell-printf (str &optional args)
+  "Return the result of printing STR in the user's shell.
+
+Executes $SHELL as interactive login shell.
+
+STR is inserted literally in a single-quoted argument to printf,
+and may therefore contain backslashed escape sequences understood
+by printf.
+
+ARGS is an optional list of args which will be inserted by printf
+in place of any % placeholders in STR. ARGS are not automatically
+shell-escaped, so they may contain $ etc."
+  (let* ((printf-bin (or (executable-find "printf") "printf"))
+         (printf-command
+          (concat printf-bin
+                  " '__RESULT\\000" str "' "
+                  (mapconcat #'import-env-from-shell--double-quote args " ")))
+         (shell-args (append import-env-from-shell-arguments
+                             (list "-c"
+                                   (if (import-env-from-shell--standard-shell-p (getenv "SHELL"))
+                                       printf-command
+                                     (concat "sh -c " (shell-quote-argument printf-command))))))
+         (shell (getenv "SHELL")))
+    (with-temp-buffer
+      (import-env-from-shell--debug "Invoking shell %s with args %S" shell shell-args)
+      (let ((exit-code (apply #'call-process shell nil t nil shell-args)))
+        (import-env-from-shell--debug "Shell printed: %S" (buffer-string))
+        (unless (zerop exit-code)
+          (error "Non-zero exit code from shell %s invoked with args %S.  Output was:\n%S"
+                 shell shell-args (buffer-string))))
+      (goto-char (point-min))
+      (if (re-search-forward "__RESULT\0\\(.*\\)" nil t)
+          (match-string 1)
+        (error "Expected printf output from shell, but got: %S" (buffer-string))))))
+
+(defun import-env-from-shell-getenvs (names)
+  "Get the environment variables with NAMES from the user's shell.
+
+Execute $SHELL according to `import-env-from-shell-arguments'.
+The result is a list of (NAME . VALUE) pairs."
+  (let* ((dollar-names (mapcar (lambda (n) (format "${%s-}" n)) names))
+         (values (split-string (import-env-from-shell-printf
+                                (mapconcat #'identity (make-list (length names) "%s") "\\000")
+                                dollar-names) "\0")))
+    (let (result)
+      (while names
+        (prog1
+            (push (cons (car names) (car values)) result)
+          (setq values (cdr values)
+                names (cdr names))))
+      result)))
 
 (defun import-env-from-shell-getenv (name)
   "Get the environment variable NAME from the user's shell.
 
 Execute $SHELL as interactive login shell, have it output the
 variable of NAME and return this output as string."
-  (with-temp-buffer
-    (call-process (getenv "SHELL") nil (current-buffer) nil
-                  "-i" "-c" (concat "echo __RESULT=$" name))
-    (when (re-search-backward "__RESULT=\\(.*\\)" nil t)
-      (match-string 1))))
+  (cdr (assoc name (import-env-from-shell-getenvs (list name)))))
+
+(defun import-env-from-shell-setenv (name value)
+  "Set the value of environment var NAME to VALUE.
+Additionally, if NAME is \"PATH\" then also set corresponding
+variables such as `exec-path'."
+  (setenv name value)
+  (when (string-equal "PATH" name)
+    (setq eshell-path-env value
+          exec-path (append (parse-colon-path value) (list exec-directory)))))
+
+(defun import-env-from-shell-copy-envs (names)
+  "Set the environment variables with NAMES from the user's shell.
+
+As a special case, if the variable is $PATH, then `exec-path' and
+`eshell-path-env' are also set appropriately.  The result is an alist,
+as described by `import-env-from-shell-getenvs'."
+  (mapc (lambda (pair)
+          (import-env-from-shell-setenv (car pair) (cdr pair)))
+        (import-env-from-shell-getenvs names)))
 
 (defun import-env-from-shell-copy-env (name)
   "Set the environment variable $NAME from the user's shell.
@@ -54,11 +154,7 @@ As a special case, if the variable is $PATH, then `exec-path' and
 `eshell-path-env' are also set appropriately.  Return the value
 of the environment variable."
   (interactive "sCopy value of which environment variable from shell? ")
-  (prog1
-      (setenv name (import-env-from-shell-getenv name))
-    (when (string-equal "PATH" name)
-      (setq eshell-path-env (getenv "PATH")
-            exec-path (split-string (getenv "PATH") path-separator)))))
+  (cdar (import-env-from-shell-copy-envs (list name))))
 
 (defun import-env-from-shell-initialize ()
   "Initialize environment from the user's shell.
@@ -67,7 +163,7 @@ The values of all the environment variables named in
 `import-env-from-shell-variables' are set from the corresponding
 values used in the user's shell."
   (interactive)
-  (mapc 'import-env-from-shell-copy-env import-env-from-shell-variables))
+  (import-env-from-shell-copy-envs import-env-from-shell-variables))
 
 (import-env-from-shell-initialize)
 
