6262e780656bf046196ca51f59b0e518d270b35c
vigou3 <vigou3@5a16c0e7-0f79-4c8a-b882-1e6d53282a5a>
2015-06-05 15:04:27 +0000

    Tag version 24.5-1-modified-1

---
+++ trunk/default.el
@@ -0,0 +1,41 @@
+;;; default.el --- Default configuration for GNU Emacs on OS X
+;;; Used mainly to load custom extensions.
+;;; (Loaded *after* any user and site configuration files)
+
+;; Copyright (C) 2015 Vincent Goulet
+
+;; Author: Vincent Goulet
+
+;; This file is part of GNU Emacs.app Modified
+;; http://vgoulet.act.ulaval.ca/emacs
+
+;; GNU Emacs.app Modified is free software; you can redistribute it
+;; and/or modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;;
+;;; Version number of Emacs Modified
+;;;
+;; Define variable and function 'emacs-modified-version'
+(require 'version-modified)
+
+;;;
+;;; Import the shell environment
+;;;
+;; Emacs does not always properly catch the system and user paths and
+;; other environment variable at launch on OS X. Forcibly import some
+;; environment variables here; see 'import-env-from-shell-variables'
+;; for the list. You can customize this variable in site-start.el or
+;; the user's config file.
+(require 'import-env-from-shell)
