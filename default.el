948b1d89cac6f15c9bb53144cfc467914055778f
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2018-08-18 00:25:21 -0400

    reworked comments in headers

--- default.el
+++ default.el
@@ -1,24 +1,25 @@
 ;;; default.el --- Default configuration for Emacs Modified for macOS
-;;; Used mainly to load custom extensions.
-;;; (Loaded *after* any user and site configuration files)
-
+;;
+;; This file is used mainly to load custom extensions. It is loaded
+;; *after* any user and site configuration files.
+;;
 ;; Copyright (C) 2017 Vincent Goulet
-
+;;
 ;; Author: Vincent Goulet
-
+;;
 ;; This file is part of Emacs Modified for macOS
-;; https://vigou3.github.io/emacs-modified-macos
+;; https://gitlab.com/vigou3/emacs-modified-macos
 
 ;; Emacs Modified for macOS is free software; you can redistribute it
 ;; and/or modify it under the terms of the GNU General Public License
 ;; as published by the Free Software Foundation; either version 3, or
 ;; (at your option) any later version.
-
+;;
 ;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs; see the file COPYING.  If not, write to the
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
--- site-start.el
+++ site-start.el
@@ -1,22 +1,22 @@
 ;;; site-start.el --- Customizations for Emacs Modified for macOS
-
+;;
 ;; Copyright (C) 2015-2017 Vincent Goulet
-
+;;
 ;; Author: Vincent Goulet
-
+;;
 ;; This file is part of Emacs Modified for macOS
-;; https://vigou3.github.io/emacs-modified-macos
+;; https://gitlab.com/vigou3/emacs-modified--macos
 
 ;; Emacs Modified for macOS is free software; you can redistribute it
 ;; and/or modify it under the terms of the GNU General Public License
 ;; as published by the Free Software Foundation; either version 3, or
 ;; (at your option) any later version.
-
+;;
 ;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs; see the file COPYING.  If not, write to the
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
